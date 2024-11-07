6099435536c34cb961a7b76e2feaf029d4a56bcb
Jeff McClintock <jef@synthedit.com>
2024-11-08 10:10:50 +1300

    chore(se) : remove refs to SE_SUPPORT_MFC

--- modules/shared/xplatform.h
+++ modules/shared/xplatform.h
@@ -11,16 +11,15 @@
 * 
   Platform				Defined
   SEM Plugin			SE_TARGET_SEM (selected SEMs)
-  AU Plugin                         					                	SE_EXTERNAL_SEM_SUPPORT=1
-  VST2/3 Plugin(64-bit)									                	SE_EXTERNAL_SEM_SUPPORT=1	SE_TARGET_VST3 (mac only)
-  JUCE Plugin			GMPI_IS_PLATFORM_JUCE==1		                	SE_EXTERNAL_SEM_SUPPORT=0
-  SynthEdit.exe			SE_EDIT_SUPPORT					SE_SUPPORT_MFC		SE_EXTERNAL_SEM_SUPPORT=1
-  SynthEdit.exe (store)	SE_EDIT_SUPPORT					SE_SUPPORT_MFC		SE_EXTERNAL_SEM_SUPPORT=1	SE_TARGET_STORE
+  AU Plugin                         					  	SE_EXTERNAL_SEM_SUPPORT=1
+  VST2/3 Plugin(64-bit)									  	SE_EXTERNAL_SEM_SUPPORT=1	SE_TARGET_VST3 (mac only)
+  JUCE Plugin			GMPI_IS_PLATFORM_JUCE==1		  	SE_EXTERNAL_SEM_SUPPORT=0
+  SynthEdit.exe			SE_EDIT_SUPPORT						SE_EXTERNAL_SEM_SUPPORT=1
+  SynthEdit.exe (store)	SE_EDIT_SUPPORT						SE_EXTERNAL_SEM_SUPPORT=1	SE_TARGET_STORE
   
   SE_TARGET_PLUGIN  - Code is running in a plugin (VST, AU, JUCE etc)
-  SE_SUPPORT_MFC    - MFC-based Serialization support.
 
-  no longer in use: SE_TARGET_AU
+  no longer in use: SE_TARGET_AU, SE_SUPPORT_MFC
 */
 
 #if !defined(GMPI_IS_PLATFORM_JUCE) // allow this to be set in cmake for SE2JUCE projects
@@ -36,20 +35,6 @@
 	#define SE_EXTERNAL_SEM_SUPPORT 1
 #else
 	#define SE_EXTERNAL_SEM_SUPPORT 0
-#endif
-
-//#if GMPI_IS_PLATFORM_JUCE==1
-//	// The SDK references this macro (without including xplatform.h), so you need to explicity define it in the IDE/Build system.
-//	#if !defined(SE_TARGET_PLU GIN)
-//		#error please define SE_TARGET_PLU GIN
-//	#endif
-//#endif
-
-// MFC Support
-#if defined( SE_EDIT_SUPPORT ) && !defined(SE_WINUI3)
-#if !defined(SE_SUPPORT_MFC)
-#error please define SE_SUPPORT_MFC
-#endif
 #endif
 
 #ifdef _WIN32
