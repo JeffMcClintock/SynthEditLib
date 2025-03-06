766b478dcc04b52e1c8cd80609747f5b80bcee45
Jeff McClintock <jef@synthedit.com>
2025-03-06 16:48:10 +1300

    chore(se) : SynthEdit2 now takes it's modules from /SynthEdit2/modules and /mac_assets

--- modules/plugin_helper.cmake
+++ modules/plugin_helper.cmake
@@ -214,7 +214,7 @@ if(${BUILD_GMPI_PLUGIN_IS_OFFICIAL_MODULE})
     add_custom_command(TARGET ${BUILD_GMPI_PLUGIN_PROJECT_NAME}
     # Run after all other rules within the target have been executed
     POST_BUILD
-    COMMAND xcopy /c /y "\"$(OutDir)$(TargetName)$(TargetExt)\"" C:\\SE\\SE16\\SynthEdit\\modules64\\
+    COMMAND xcopy /c /y "\"$(OutDir)$(TargetName)$(TargetExt)\"" C:\\SE\\SE16\\SynthEdit2\\modules\\
     COMMENT "Copy to SynthEdit plugin folder"
     )
 else()
