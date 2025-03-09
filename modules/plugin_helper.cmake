f960d4c041abe2d83a1070670075a5a9a5cdd2fa
Jeff McClintock <jef@synthedit.com>
2025-03-10 11:56:32 +1300

    chore(se) : changed factory SEM output folders

--- modules/plugin_helper.cmake
+++ modules/plugin_helper.cmake
@@ -215,7 +215,7 @@ if(${BUILD_GMPI_PLUGIN_IS_OFFICIAL_MODULE})
     add_custom_command(TARGET ${BUILD_GMPI_PLUGIN_PROJECT_NAME}
     # Run after all other rules within the target have been executed
     POST_BUILD
-    COMMAND xcopy /c /y "\"$(OutDir)$(TargetName)$(TargetExt)\"" "\"C:\\SE\\SE16\\SynthEdit2\\mac_assets\\$(TargetName)\\$(TargetName)$(TargetExt)\\Contents\\x86_64-win\\\""
+    COMMAND xcopy /c /y "\"$(OutDir)$(TargetName)$(TargetExt)\"" "\"C:\\SE\\SE16\\SynthEdit2\\mac_assets\\$(TargetName)$(TargetExt)\\Contents\\x86_64-win\\\""
     COMMENT "Copy to SynthEdit plugin folder"
     )
 else()
