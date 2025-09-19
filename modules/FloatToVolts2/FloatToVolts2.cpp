97d21895e295668c68582423c3429a898cc11b32
Jeff McClintock <jef@synthedit.com>
2025-09-19 15:09:16 +1200

    feat(se) : new Float to Volts2 module, more smooth.

--- modules/FloatToVolts2/FloatToVolts2.cpp
+++ modules/FloatToVolts2/FloatToVolts2.cpp
@@ -13,124 +13,20 @@ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 #include "Processor.h"
+#include "..\se_sdk3\smart_audio_pin.h" // for the RampGeneratorAdaptive
 
 using namespace gmpi;
 
-class RampGeneratorAdaptive2 // copied from SDK3
-{
-	double adaptiveHi_ = {};
-	double adaptiveLo_ = {};
-
-public:
-	RampGeneratorAdaptive2() :
-		currentValue_((std::numeric_limits<double>::max)())
-		, dv(0.0)
-	{
-	}
-
-	void Init(float sampleRate)
-	{
-		inverseTransitionTime_ = 1.0 / (sampleRate * 0.015); // 15ms default.
-
-		adaptiveLo_ = 1.0 / (sampleRate * 0.050); // 50ms max.
-		adaptiveHi_ = 1.0 / (sampleRate * 0.001); // 1ms min.
-	}
-
-	void setTarget(float targetValue)
-	{
-		if (currentValue_ == (std::numeric_limits<double>::max)())
-		{
-			currentValue_ = targetValue_ = targetValue;
-			dv = 0.0;
-			return;
-		}
-
-		if (currentValue_ == targetValue_) // too fast, slow down a bit.
-		{
-			if (inverseTransitionTime_ > adaptiveLo_)
-			{
-				inverseTransitionTime_ *= 0.9;
-			}
-		}
-		else
-		{
-			if (inverseTransitionTime_ < adaptiveLo_)
-			{
-				inverseTransitionTime_ *= 1.05; // slower 'decay', kind of peak follower.
-			}
-		}
-
-		targetValue_ = targetValue;
-		dv = (targetValue_ - currentValue_) * inverseTransitionTime_;
-	}
-
-	void setValueInstant(float targetValue)
-	{
-		currentValue_ = targetValue_ = targetValue;
-		dv = 0.0;
-	}
-
-	float getInstantValue() const
-	{
-		return static_cast<float>(currentValue_);
-	}
-
-	float getTargetValue() const
-	{
-		return static_cast<float>(targetValue_);
-	}
-
-	inline float getNext()
-	{
-		currentValue_ += dv;
-
-		if (dv > 0.0)
-		{
-			if (currentValue_ >= targetValue_)
-			{
-				currentValue_ = targetValue_;
-				dv = 0.0;
-			}
-		}
-		else
-		{
-			if (currentValue_ <= targetValue_)
-			{
-				currentValue_ = targetValue_;
-				dv = 0.0;
-			}
-		}
-
-		return static_cast<float>(currentValue_);
-	}
-
-	inline bool isDone()
-	{
-		return dv == 0.0;
-	}
-
-	void jumpToTarget()
-	{
-		setValueInstant(static_cast<float>(targetValue_));
-	}
-private:
-	double dv = {};
-	double currentValue_ = {};
-	double targetValue_ = {};
-	double inverseTransitionTime_ = {};
-};
-
 struct FloatToVolts2 final : public Processor
 {
-	IntInPin pinSmoothing;
-	FloatInPin pinFloatIn;
-	AudioOutPin pinVoltsOut;
+	gmpi::IntInPin pinSmoothing;
+	gmpi::FloatInPin pinFloatIn;
+	gmpi::AudioOutPin pinVoltsOut;
 
-	RampGeneratorAdaptive2 smoother;
+	RampGeneratorAdaptive smoother;
+	float smoothTimeSamples{ -1 };
 
-	FloatToVolts2()
-	{
-	}
+	FloatToVolts2() = default;
 
 	gmpi::ReturnCode open(api::IUnknown* phost) override
 	{
@@ -148,25 +44,56 @@ struct FloatToVolts2 final : public Processor
 			pinVoltsOut.setStreaming(false, 0);
 		}
 
-		// get pointers to in/output buffers.
 		auto voltsOut = getBuffer(pinVoltsOut);
 
 		for( int s = sampleFrames; s > 0; --s )
 		{
-			// TODO: Signal processing goes here.
 			*voltsOut++ = smoother.getNext();
 		}
 	}
 
 	void onSetPins() override
 	{
-		// Check which pins are updated.
-		if( pinSmoothing.isUpdated() )
+		if(pinSmoothing.isUpdated() )
 		{
+			const auto target = 0.1f * pinFloatIn.getValue();
+			switch (pinSmoothing.getValue())
+			{
+			default:
+				smoothTimeSamples = -1; // N/A
+				break;
+
+			case 1: // Fast
+				smoothTimeSamples = 4.0f;
+				break;
+
+			case 2: // Smooth
+				smoothTimeSamples = 0.03f * host->getSampleRate();
+				break;
+
+			case 3: // None
+				smoothTimeSamples = 0.0f; // N/A
+				break;
+			}
 		}
-		if( pinFloatIn.isUpdated() )
+		if (pinFloatIn.isUpdated())
 		{
-			smoother.setTarget(0.1f * pinFloatIn.getValue());
+			const auto target = 0.1f * pinFloatIn.getValue();
+			switch (pinSmoothing.getValue())
+			{
+			default:
+				smoother.setTarget(target);
+				break;
+
+			case 1: // Fast
+			case 2: // Smooth
+				smoother.SetTargetWithTimeInSamples(target, smoothTimeSamples);
+				break;
+
+			case 3: // None
+				smoother.setValueInstant(target);
+				break;
+			}
 		}
 
 		// Set state of output audio pins.
@@ -179,12 +106,12 @@ struct FloatToVolts2 final : public Processor
 
 namespace
 {
-auto r = Register<FloatToVolts2>::withXml(R"XML(
+auto r = gmpi::Register<FloatToVolts2>::withXml(R"XML(
 <?xml version="1.0" encoding="UTF-8"?>
 <PluginList>
     <Plugin id="SE Float to Volts2" name="Float to Volts2" category="Debug">
         <Audio>
-            <Pin name="Smoothing" datatype="enum" default="2" metadata="None,Fast (4 samp),Smooth (30ms)"/>
+            <Pin name="Smoothing" datatype="enum" metadata="Auto, Fast (4 samp), Smooth (30ms), None"/>
             <Pin name="Float In" datatype="float"/>
             <Pin name="Volts Out" datatype="float" rate="audio" direction="out"/>
         </Audio>
