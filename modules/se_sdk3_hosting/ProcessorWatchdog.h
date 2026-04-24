997f6e92f28b76935680dfb8e46c2c26f5a79155
Jeff McClintock <jef@synthedit.com>
2026-04-24 13:29:16 +1200

    remove WATCHDOG_DEBUG_LOG macro and all uses

--- modules/se_sdk3_hosting/ProcessorWatchdog.h
+++ modules/se_sdk3_hosting/ProcessorWatchdog.h
@@ -6,22 +6,6 @@
 
 #include <functional>
 
-#ifdef _DEBUG
-#include <string>
-#if defined(_WIN32)
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#include <windows.h>
-#define WATCHDOG_DEBUG_LOG(msg) OutputDebugStringA(msg)
-#else
-#include <cstdio>
-#define WATCHDOG_DEBUG_LOG(msg) fprintf(stderr, "%s", msg)
-#endif
-#else
-#define WATCHDOG_DEBUG_LOG(msg) ((void)0)
-#endif
-
 // Monitors DSP processor activity and notifies when processor goes offline/online.
 // The watchdog timer counts down on each tick; if it reaches zero without being reset,
 // the processor is considered offline. When a message is received, the counter resets
@@ -37,15 +21,11 @@ public:
 		: timerInit(1000 / timerPeriodMs)
 		, counter(timerInit)
 	{
-		WATCHDOG_DEBUG_LOG(("ProcessorWatchdog: ctor timerPeriodMs=" + std::to_string(timerPeriodMs) +
-			" timerInit=" + std::to_string(timerInit) + "\n").c_str());
 	}
 
 	// Set the callback to be invoked when offline status changes
 	void setCallback(OfflineCallback callback)
 	{
-		WATCHDOG_DEBUG_LOG(("ProcessorWatchdog: setCallback hasCallback=" +
-			std::to_string(callback != nullptr) + "\n").c_str());
 		onOfflineChanged = std::move(callback);
 		
 		// need to initialize the caller to offline state. (if we are actually online, this will be corrected soon enough).
@@ -61,12 +41,7 @@ public:
 		{
 			if(onOfflineChanged)
 			{
-				WATCHDOG_DEBUG_LOG("ProcessorWatchdog: onDspMessage -> ONLINE (was offline)\n");
 				onOfflineChanged(false); // processor is online
-			}
-			else
-			{
-				WATCHDOG_DEBUG_LOG("ProcessorWatchdog: onDspMessage -> ONLINE (no one listening)\n");
 			}
 		}
 		counter = timerInit;
@@ -79,12 +54,7 @@ public:
 		{
 			if(onOfflineChanged)
 			{
-				WATCHDOG_DEBUG_LOG("ProcessorWatchdog: onTimerTick -> OFFLINE\n");
 				onOfflineChanged(true); // processor is offline
-			}
-			else
-			{
-				WATCHDOG_DEBUG_LOG("ProcessorWatchdog: onTimerTick -> OFFLINE (no one listening)\n");
 			}
 		}
 	}
