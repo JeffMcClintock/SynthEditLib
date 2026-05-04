2d7cd7546544cce91ab620697c05537d543bad51
Jeff McClintock <jef@synthedit.com>
2026-05-04 16:09:37 +1200

    ci

--- modules/se_sdk3_hosting/SynthEditCocoaView.mm
+++ modules/se_sdk3_hosting/SynthEditCocoaView.mm
@@ -361,7 +361,7 @@ public:
         if (!inputClient)
             return gmpi::ReturnCode::Unhandled;
 
-        gmpi::drawing::Rect rect(point.x, point.y, point.x + 120, point.y + 20);
+        gmpi::drawing::Rect rect{point.x, point.y, point.x + 120, point.y + 20};
 
         gmpi::shared_ptr<gmpi::api::IUnknown> unknown;
         if (createPopupMenu(&rect, unknown.put()) != gmpi::ReturnCode::Ok)
